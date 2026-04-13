cdf59fa34b484d15c7ea4705ed2cfed80c867175
Ammar <alsoammar1812@gmail.com>
2026-04-13 07:51:53 +0200

    Initial Commit

---
+++ src/TeamSearcher/TeamSearcher.Android/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia;
+using Avalonia.Android;
+
+namespace TeamSearcher.Android;
+
+[Activity(
+    Label = "TeamSearcher.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity
+{
+}
