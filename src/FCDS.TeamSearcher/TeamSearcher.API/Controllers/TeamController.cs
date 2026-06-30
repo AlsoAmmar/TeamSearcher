4e6ea74c2c4a2d2c3ab244e8da198864b1611aa5
Ammar <alsoammar1812@gmail.com>
2026-06-30 13:41:00 +0300

    Added Team Controller

--- src/FCDS.TeamSearcher/TeamSearcher.API/Controllers/TeamController.cs
+++ src/FCDS.TeamSearcher/TeamSearcher.API/Controllers/TeamController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using TeamSearcher.Application.Contracts.Services;
+using TeamSearcher.Application.DTOs.Team;
 
 namespace TeamSearcher.API.Controllers;
 
@@ -13,6 +14,32 @@ public class TeamController : ControllerBase
     {
         _teamService = teamService;
     }
-    
-    
+
+    [HttpGet("{teamId}")]
+    public async Task<IActionResult> GetRequests(int teamId, CancellationToken cancellationToken)
+    {
+        var persons = await _teamService.GetPersonRequests(teamId, cancellationToken);
+        return Ok(persons);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTeam([FromBody] CreateTeamDto team, CancellationToken cancellationToken)
+    {
+        var teamId = await _teamService.CreateTeamAsync(team, cancellationToken);
+        return Ok(teamId);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateTeam([FromBody] UpdateTeamDto team, CancellationToken cancellationToken)
+    {
+        await _teamService.UpdateTeam(team, cancellationToken);
+        return Ok();
+    }
+
+    [HttpPut("{teamId}/{personId}")]
+    public async Task<IActionResult> AcceptRequest(int personId, int teamId, CancellationToken cancellationToken)
+    {
+        await _teamService.AcceptRequest(personId, teamId, cancellationToken);
+        return Ok();
+    }
 }
