45d7b38a6eead5b4b4f3abf80e1b3cd6ede5491a
Ammar <alsoammar1812@gmail.com>
2026-06-30 12:37:52 +0300

    Added API Project and controllers

---
+++ src/FCDS.TeamSearcher/TeamSearcher.API/Controllers/PersonController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamSearcher.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class PersonController : ControllerBase
+{
+    
+}
