ae9b1c7dba56dd1e5e8433b209f4c94b9a119206
Ammar <alsoammar1812@gmail.com>
2026-08-17 12:16:11 +0300

    Added mappings
    Why is this so annoying to figure out?

--- src/FCDS.TeamSearcher.Py/TeamSearcher_Application/DTOs/person_dtos.py
+++ src/FCDS.TeamSearcher.Py/TeamSearcher_Application/DTOs/person_dtos.py
@@ -1,15 +1,34 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from TeamSearcher_Domain import Person
 
 class PersonDto(BaseModel):
     id: int
     name: str
     number: int
 
+    def to_entity(self) -> "Person":
+        return Person(
+            id = self.id,
+            name = self.name,
+            number = self.number
+        )
+
 class CreatePersonDto(BaseModel):
     name: str
     number: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+    @classmethod
+    def to_dto(cls, person: Person) -> "CreatePersonDto":
+        return cls.model_validate(person)
+
 
 class UpdatePersonDto(BaseModel):
     id: int
     name: str
     number: str
+
+    def update_person(self, person: Person):
+        person.name = self.name
+        person.number = self.number
